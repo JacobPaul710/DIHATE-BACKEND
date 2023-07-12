6059b29fbe8dc47b58ca124ff25ffe1d9626029b
JacobPaul710 <jacobpaulscott@outlook.com>
2023-07-12 08:45:38 -0700

    Connects Mongoose to MongoDB.

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const connectionString = process.env.MONGO_DB_URI;
+
+mongoose.connect(connectionString);
+console.log(connectionString);
+
+mongoose.connection.on('connected', () => {
+    console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected...🥗🍲 `)
+})
+
+mongoose.connection.on('error', (error) => {
+    console.log('MongoDB connection error 😳👎', error)
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected 🪫🚫')
+})
+
