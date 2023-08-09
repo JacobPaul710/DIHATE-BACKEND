c7c83a09874249503c94dca41ade085fc36ead96
JacobPaul710 <jacobpaulscott@outlook.com>
2023-08-09 08:19:45 -0700

    Updates cors to use full domain name.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'jacobscott.tech',
+    origin: 'https://www.jacobscott.tech',
     credentials: true,
     exposedHeaders: ["set-cookie"]
 }
